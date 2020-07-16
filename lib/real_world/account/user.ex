853b9af866cf548909359db831e77dc4af3922f1
mihkelal <mihkel.alavere@eesti.ee>
2020-07-16 14:58:04 +0300

    Add generated user code

---
+++ lib/real_world/account/user.ex
@@ -0,0 +1,21 @@
+defmodule RealWorld.Account.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :bio, :string
+    field :email, :string
+    field :image, :string
+    field :password, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email, :password, :bio, :image])
+    |> validate_required([:username, :email, :password, :bio, :image])
+  end
+end
---
+++ lib/real_world_web/views/user_view.ex
@@ -0,0 +1,21 @@
+defmodule RealWorldWeb.UserView do
+  use RealWorldWeb, :view
+  alias RealWorldWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      bio: user.bio,
+      image: user.image}
+  end
+end
