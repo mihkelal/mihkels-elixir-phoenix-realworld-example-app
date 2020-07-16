de59b22b470d876186aa4f7ec92d0b1de8ef27cd
mihkelal <mihkel.alavere@eesti.ee>
2020-07-16 22:15:07 +0300

    Fix user changeset

--- lib/real_world/account/user.ex
+++ lib/real_world/account/user.ex
@@ -15,7 +15,9 @@ defmodule RealWorld.Account.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :email, :password, :bio, :image])
-    |> validate_required([:username, :email, :password, :bio, :image])
+    |> cast(attrs, [:username, :email, :password])
+    |> validate_required([:username, :email, :password])
+    |> unique_constraint(:username)
+    |> unique_constraint(:email)
   end
 end
