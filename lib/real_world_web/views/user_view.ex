d52755791b222597f7344eacc087e56d6daef819
mihkelal <mihkel.alavere@eesti.ee>
2020-07-16 22:17:46 +0300

    Correct user response

--- lib/real_world_web/views/user_view.ex
+++ lib/real_world_web/views/user_view.ex
@@ -3,11 +3,11 @@ defmodule RealWorldWeb.UserView do
   alias RealWorldWeb.UserView
 
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, UserView, "user.json")}
+    %{users: render_many(users, UserView, "user.json")}
   end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, UserView, "user.json")}
+    %{user: render_one(user, UserView, "user.json")}
   end
 
   def render("user.json", %{user: user}) do
