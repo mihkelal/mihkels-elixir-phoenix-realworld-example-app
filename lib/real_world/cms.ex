26508cc5d9a7267b5a2496d168cef3ef5ec59c17
mihkelal <mihkel.alavere@eesti.ee>
2021-03-22 23:26:40 +0200

    Implement feed

--- lib/real_world/cms.ex
+++ lib/real_world/cms.ex
@@ -9,28 +9,40 @@ defmodule RealWorld.CMS do
   alias RealWorld.Repo
 
   alias RealWorld.Account
+  alias RealWorld.Account.Following
   alias RealWorld.Account.User
   alias RealWorld.CMS.Article
 
-  def list_articles(author: author, limit: limit, offset: offset) do
-    limit = limit || @default_article_pagination_limit
-    offset = offset || @default_article_offset
-
-    case author do
-      nil ->
-        Article
-
-      author ->
-        author
-        |> Account.get_user_by_username()
-        |> Ecto.assoc(:articles)
+  def list_articles(%{} = params \\ %{}) do
+    case params[:author] && Account.get_user_by_username(params[:author]) do
+      %User{} = user -> Ecto.assoc(user, :articles)
+      nil -> Article
     end
+    |> limit(^params[:limit])
+    |> offset(^params[:offset])
+    |> default_articles_list_options
+    |> Repo.all()
+  end
+
+  def list_feed(%{user: user} = params \\ %{}) do
+    from(a in Article,
+      join: f in Following,
+      on: a.user_id == f.followee_id,
+      where: f.follower_id == ^user.id
+    )
+    |> limit(^params[:limit])
+    |> offset(^params[:offset])
+    |> default_articles_list_options
+    |> Repo.all()
+  end
+
+  defp default_articles_list_options(query) do
+    query
     |> join(:inner, [a], u in User, on: a.user_id == u.id)
     |> preload(:user)
-    |> limit(^limit)
-    |> offset(^offset)
+    |> limit(@default_article_pagination_limit)
+    |> offset(@default_article_offset)
     |> order_by(desc: :inserted_at)
-    |> Repo.all()
   end
 
   def get_article!(id), do: Repo.get!(Article, id)
--- lib/real_world_web/controllers/article_controller.ex
+++ lib/real_world_web/controllers/article_controller.ex
@@ -5,8 +5,18 @@ defmodule RealWorldWeb.ArticleController do
 
   action_fallback RealWorldWeb.FallbackController
 
+  plug Guardian.Plug.EnsureAuthenticated when action in [:feed]
+
   def index(conn, %{"limit" => limit, "offset" => offset} = params) do
-    articles = CMS.list_articles(author: params["author"], limit: limit, offset: offset)
+    articles = CMS.list_articles(%{author: params["author"], limit: limit, offset: offset})
+
+    render(conn, "index.json", articles: articles)
+  end
+
+  def feed(conn, %{"limit" => limit, "offset" => offset} = _params) do
+    current_user = RealWorldWeb.Guardian.Plug.current_resource(conn)
+
+    articles = CMS.list_feed(%{user: current_user, limit: limit, offset: offset})
 
     render(conn, "index.json", articles: articles)
   end
