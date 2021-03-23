2b019310ee2a4b2dd72340b85fc0aecf1920eda8
mihkelal <mihkel.alavere@eesti.ee>
2021-03-23 13:34:36 +0200

    Add controller action and cms method and update schema

--- lib/real_world_web/controllers/article_controller.ex
+++ lib/real_world_web/controllers/article_controller.ex
@@ -1,12 +1,13 @@
 defmodule RealWorldWeb.ArticleController do
   use RealWorldWeb, :controller
 
+  alias RealWorld.Repo
   alias RealWorld.CMS
   alias RealWorld.CMS.Article
 
   action_fallback RealWorldWeb.FallbackController
 
-  plug Guardian.Plug.EnsureAuthenticated when action in [:feed]
+  plug Guardian.Plug.EnsureAuthenticated when action in [:feed, :create]
 
   def index(conn, %{"limit" => limit, "offset" => offset} = params) do
     articles = CMS.list_articles(%{author: params["author"], limit: limit, offset: offset})
@@ -18,6 +19,24 @@ defmodule RealWorldWeb.ArticleController do
     case CMS.get_article_by_slug!(slug) do
       %Article{} = article -> render(conn, "show.json", article: article)
       nil -> send_resp(conn, 404, "")
+    end
+  end
+
+  def create(conn, %{"article" => article_params} = _params) do
+    user = RealWorldWeb.Guardian.Plug.current_resource(conn)
+
+    article_params = Map.put(article_params, "user_id", user.id)
+
+    case CMS.create_article(article_params) do
+      {:ok, article} ->
+        conn
+        |> put_status(:created)
+        |> render("show.json", article: Repo.preload(article, :user))
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render("error.json", message: inspect(changeset.errors))
     end
   end
 
